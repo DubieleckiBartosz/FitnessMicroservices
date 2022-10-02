4589db6d404ea442bb16429d291ecf7a064c3bd9
Ba348 <bdubielecki@gmail.com>
2022-10-02 02:00:58 +0200

    Database with docker have been created

---
+++ Identity-create-tables.sql
@@ -0,0 +1,71 @@
+﻿IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='ApplicationUsers' and xtype='U')
+BEGIN
+
+	CREATE TABLE ApplicationUsers
+		(
+		    Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL, 
+		    FirstName VARCHAR(50) NOT NULL,
+			LastName VARCHAR(50) NOT NULL,
+			UserName VARCHAR(50) NOT NULL,
+		    Email VARCHAR(50) NULL,
+		    PasswordHash VARCHAR(MAX) NULL,
+		    PhoneNumber VARCHAR(50) NULL,
+			Created DATETIME NOT NULL DEFAULT GETDATE(),
+			Modified DATETIME NOT NULL DEFAULT GETDATE()
+		)
+END
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='RefreshTokens' and xtype='U')
+BEGIN
+		CREATE TABLE [dbo].[RefreshTokens]
+		(
+			Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+			UserId INT NOT NULL,
+			Token VARCHAR(MAX) NULL,
+			Expires DATETIME NOT NULL,
+			Created DATETIME NOT NULL,
+			ReplacedByToken VARCHAR(MAX) NULL,
+			Revoked VARCHAR(250) NULL
+		)
+		
+		ALTER TABLE [dbo].[RefreshTokens] ADD  CONSTRAINT [FK_RefreshToken_ApplicationUsers] FOREIGN KEY([UserId])
+		REFERENCES [dbo].[ApplicationUsers] ([Id]) 
+
+		
+--TYPE
+		
+		CREATE TYPE [dbo].[UserRefreshTokensTableType] AS TABLE
+		(
+			Token VARCHAR(max) NULL,
+			Expires DATETIME NOT NULL,
+			Created DATETIME NOT NULL,
+			ReplacedByToken VARCHAR(max) NULL,
+			Revoked VARCHAR(250) NULL	
+		)
+
+END
+
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Roles' and xtype='U')
+BEGIN
+		CREATE TABLE [dbo].[Roles]
+			(
+				Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+				RoleName VARCHAR(25) NULL,
+			)
+
+		INSERT INTO Roles VALUES('Admin'), ('User'), ('Trainer')
+								
+END
+
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='UserRoles' and xtype='U')
+BEGIN
+		CREATE TABLE [dbo].[UserRoles]
+		(
+			UserId INT NOT NULL FOREIGN KEY REFERENCES ApplicationUsers(Id) ON DELETE CASCADE,
+			RoleId INT NOT NULL FOREIGN KEY REFERENCES Roles(Id) ON DELETE CASCADE,
+		    CONSTRAINT PK_Users_Roles PRIMARY KEY(UserId, RoleId)
+		)
+END
+
---
+++ sql.Dockerfile
@@ -0,0 +1,18 @@
+FROM mcr.microsoft.com/mssql/server
+ 
+USER root
+
+ARG PROJECT_DIR=/tmp/mssql-scripts
+
+RUN mkdir -p $PROJECT_DIR
+WORKDIR $PROJECT_DIR
+ENV SA_PASSWORD=a?i0/cEFB@v3dweF7C
+ENV ACCEPT_EULA=Y
+COPY Identity-create-db.sql ./
+COPY Identity-create-tables.sql ./
+COPY Identity-create-storedProcedures.sql ./
+COPY entrypoint.sh ./
+COPY setupsql.sh ./
+
+RUN chmod +x setupsql.sh
+CMD ["/bin/bash", "entrypoint.sh"]
