43077456ed05538dcb0ac8c3f48ac2f89ade02e6
Ba348 <bdubielecki@gmail.com>
2022-10-25 01:06:50 +0200

    UX line endings

--- src/Services/Training/Training.API/Commands/TrainingCommands/TrainingInitiationCommand.cs
+++ src/Services/Training/Training.API/Commands/TrainingCommands/TrainingInitiationCommand.cs
@@ -4,7 +4,7 @@ namespace Training.API.Commands.TrainingCommands
 {
     public record TrainingInitiationCommand(Guid TrainerId) : ICommand<Guid>
     {
-        public static TrainingInitiationCommand Create(Guid trainerId)
+        public static TrainingInitiationCommand Create(Guid trainerId) 
         {
             return new TrainingInitiationCommand(trainerId);
         }
