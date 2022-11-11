df7a4894618e015bbb84ac802d004f19706a3004
Ba348 <bdubielecki@gmail.com>
2022-11-11 11:00:06 +0100

    new migration - third version

--- src/Services/Training/Training.API/Migrations/20221111095934_Init-3.Designer.cs
+++ src/Services/Training/Training.API/Migrations/20221111095934_Init-3.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Training.API.Database;
@@ -11,9 +12,10 @@ using Training.API.Database;
 namespace Training.API.Migrations
 {
     [DbContext(typeof(TrainingContext))]
-    partial class TrainingContextModelSnapshot : ModelSnapshot
+    [Migration("20221111095934_Init-3")]
+    partial class Init3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
