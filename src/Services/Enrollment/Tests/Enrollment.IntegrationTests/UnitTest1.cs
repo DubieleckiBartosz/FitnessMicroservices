56ead1f95ebf3b3c2b57d3cda413dc732f78b2f4
Ba348 <bdubielecki@gmail.com>
2022-12-24 16:28:35 +0100

    Test projects added to services

---
+++ src/Services/Enrollment/Tests/Enrollment.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Enrollment.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/Services/Training/Tests/Training.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Training.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
